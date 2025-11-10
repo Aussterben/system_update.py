9b7eb5fcff6540d3ff643ff5665069ddff2f9411
Aussterben <aitorlinares11@gmail.com>
2025-11-10 22:34:07 +0100

    Add files via upload

--- system_update.py
+++ system_update.py
@@ -5,9 +5,12 @@ import os
 import time
 import sys
 
+### Url
+url_github = "https://github.com/Aussterben"
+
 # Esto me lo ha hecho chatgpt
 if os.geteuid() != 0:
-    sys.stderr.write("\n[!] Este script requiere privilegios de root. Ejecuta: sudo {}\n".format(" ".join([sys.executable]+sys.argv)))
+    print(f"\n[+] Este script requiere de permisos de administrador (usuario root)")
     sys.exit(1)
 
 # Introducción
@@ -35,7 +38,10 @@ if eleccion == "S" or eleccion == "s" or eleccion == "Y" or eleccion == "y":
     os.system("\nsudo apt update 2>/dev/null && parrot-upgrade 2>/dev/null")
     time.sleep(0.5)
     print(f"\n[+] ¡Sistema actualizado!")
+    print(f"[+] Más herramientas en github: {url_github}")
 elif eleccion == "N" or eleccion == "n":
     print(f"\n[+] No se actualizará el sistema")
+    print(f"[+] Más herramientas en github: {url_github}")
 else:
     print(f"\n[!] Elige entre 'S' o 'n'")
+    print(f"[+] Más herramientas en github: {url_github}")
